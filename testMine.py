eb3b05760cf32ace5e508ac9d20dd87f76f22ac0
Phillip G. Bradford <phillip.g.bradford@gmail.com>
2021-01-30 10:34:38 -0500

    gets difficulty from command line

--- testMine.py
+++ testMine.py
@@ -1,6 +1,8 @@
+import sys
 from Block import *
 
 b=Block("empty","genesis block")
 
-newBlock=b.mineBlock(5)
+difficulty = int(sys.argv[1])
+newBlock=b.mineBlock(difficulty)
 print("start:"+newBlock.bHash+":end")
